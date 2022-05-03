5a949a1d0fa77e01d104a42b996903c73f4779d0
Alendre Lopes <alendrelopes@gmail.com>
2022-05-03 18:51:43 -0300

    Update index temporaly

---
+++ vendor/composer/installed.php
@@ -0,0 +1,23 @@
+<?php return array(
+    'root' => array(
+        'pretty_version' => 'dev-main',
+        'version' => 'dev-main',
+        'type' => 'project',
+        'install_path' => __DIR__ . '/../../',
+        'aliases' => array(),
+        'reference' => '01c58eb5714999cb7c877775c987f39ef1f35412',
+        'name' => 'alendre/alendre_cleunice',
+        'dev' => true,
+    ),
+    'versions' => array(
+        'alendre/alendre_cleunice' => array(
+            'pretty_version' => 'dev-main',
+            'version' => 'dev-main',
+            'type' => 'project',
+            'install_path' => __DIR__ . '/../../',
+            'aliases' => array(),
+            'reference' => '01c58eb5714999cb7c877775c987f39ef1f35412',
+            'dev_requirement' => false,
+        ),
+    ),
+);
